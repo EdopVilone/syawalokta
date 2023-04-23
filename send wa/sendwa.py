d923175e034c92eeb7373202080f4a966f67acea
EdopVilone <129120895+EdopVilone@users.noreply.github.com>
2023-04-24 04:42:24 +0700

    Add files via upload

---
+++ send wa/sendwa.py
@@ -0,0 +1,9 @@
+import pywhatkit
+import pyautogui
+import time
+
+pywhatkit.sendwhatmsg("nomor tujuan","auto message",4,38)
+time.sleep(1)
+pyautogui.click()
+time.sleep(1)
+pyautogui.press('enter')
